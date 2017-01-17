9e5726fbb24db34e36c6cc255d20a3d432c32a71
Nathan Martindale (WildfireXIII) <nmblenderdude0@gmail.com>
2017-01-16 22:32:16 -0600

    Experimented with some fun functions

--- base.py
+++ base.py
@@ -7,19 +7,24 @@ class Base:
     def __init__(self, generator):
         print("Importing base...")
         self.gen = generator
-        self.gen.functionList.append("add [r] [g] [b] [a]")
-        self.gen.commands["add"] = self.add
+        self.gen.functionList.append("addall [r] [g] [b] [a]")
+        self.gen.functionList.append("setall [r] [g] [b] [a]")
+        self.gen.commands["addall"] = self.addall
+        self.gen.commands["setall"] = self.setall
     
-    def add(self, r, g, b, a):
-        #self.gen
-        #print(colorVector)
+    def addall(self, r, g, b, a):
         colorVector = np.fromstring(r + " " + g + " " + b + " " + a, dtype=int, sep=' ')
         
-        print(colorVector) # DEBUG
+        print("adding " + str(colorVector) + " to all pixels...") # DEBUG
 
         self.gen.imgArray = self.gen.imgArray + colorVector
+        self.gen.imgArray = np.clip(self.gen.imgArray, 0, 255)
 
-        #for thing in colorVector:
-            #print(str(thing))
-        #for x in range(0, len(self.gen.imgArray)):
-            #for y in range(0, len(self.gen.imgArray))
+    def setall(self, r, g, b, a):
+        colorVector = np.fromstring(r + " " + g + " " + b + " " + a, dtype=int, sep=' ')
+        
+        print("setting all pixels to " + str(colorVector) + "...") # DEBUG
+        
+        for x in range(0, self.gen.imgHeight):
+            for y in range(0, self.gen.imgWidth):
+                self.gen.imgArray[x][y] = colorVector
